2f94f8ef37301c50a16d7860f6ab394bb7677cc3
mkmak2 <112261097+mkmak2@users.noreply.github.com>
2024-04-21 16:45:08 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  component: {
+module.exports = require('cypress').defineConfig({  component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
-    },
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
